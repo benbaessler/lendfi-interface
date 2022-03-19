11fa85612cef6b9d9d10587c61589f63cad1f4d3
benbaessler <mail@benbaessler.com>
2022-03-19 17:26:41 +0000

    Set up run script

---
+++ scripts/run.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Loan = await hre.ethers.getContractFactory("Loan");
+  const contract = await Loan.deploy();
+
+  await contract.deployed();
+
+  console.log("Contract deployed to:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
