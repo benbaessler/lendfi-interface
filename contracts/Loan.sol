029affc038f00c08f49588b74270db4a4b675651
benbaessler <mail@benbaessler.com>
2022-03-19 17:49:53 +0000

    Added submitLoan

--- contracts/Loan.sol
+++ contracts/Loan.sol
@@ -5,8 +5,48 @@ import "hardhat/console.sol";
 
 contract Loan {
 
-  constructor() {
-    console.log("Hello World!");
+  event SubmitLoan(
+    uint256 indexed id,
+    address indexed lender,
+    address indexed borrower,
+    uint256 amount,
+    NFT[] collateral,
+    uint256 deadline
+  );
+
+  struct NFT {
+    address _address;
+    uint256 _tokenId;
+  } 
+
+  struct Loan {
+    address lender;
+    address borrower;
+    uint256 amount;
+    NFT[] collateral;
+    uint256 deadline;
+    bool active;
   }
 
+  mapping(uint256 => mapping(address => bool)) public isConfirmed;
+
+  Loan[] public loans;
+
+  function submitLoan(address _lender, address _borrower, uint256 _amount, NFT[] memory _collateral, uint256 _deadline) public {
+    uint256 id = loans.length;
+
+    loans.push(
+      Loan({
+        lender: _lender,
+        borrower: _borrower,
+        amount: _amount,
+        collateral: _collateral,
+        deadline: _deadline,
+        active: false
+      })
+    );
+
+    emit SubmitLoan(id, _lender, _borrower, _amount, _collateral, _deadline);
+  }
+  
 }
