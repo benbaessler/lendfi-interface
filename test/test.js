9e91330af42bc897fd1c7688cda2b3c1c23e92f8
benbaessler <mail@benbaessler.com>
2022-03-25 20:22:38 +0000

    Updated tests

--- test/test.js
+++ test/test.js
@@ -4,14 +4,19 @@ const { ethers } = require("hardhat");
 describe("LoanFactory contract", () => {
 
   let contract
+  let collateral
   let user1
   let user2
+  let user3
 
   beforeEach(async () => {
-    const Loan = await ethers.getContractFactory('LoanFactory');
-    [user1, user2] = await ethers.getSigners()
+    [user1, user2, user3] = await ethers.getSigners()
 
+    const Loan = await ethers.getContractFactory('LoanFactory');
     contract = await Loan.deploy()
+
+    const Collateral = await ethers.getContractFactory('Collateral') 
+    collateral = await Collateral.deploy()
   })
 
   describe('Initiating a loan', () => {
@@ -20,8 +25,8 @@ describe("LoanFactory contract", () => {
       await contract.submitLoan(
         user1.address, 
         user2.address, 
-        100,
-        { contractAddress: '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D', tokenId: 1 },
+        ethers.utils.parseEther('.5'),
+        { contractAddress: collateral.address, tokenId: 1 },
         1234567
       )
     })
@@ -31,24 +36,47 @@ describe("LoanFactory contract", () => {
       
       expect(loan.lender).to.equal(user1.address)
       expect(loan.borrower).to.equal(user2.address)
-      expect(loan.amount).to.equal(100)
+      expect(loan.amount).to.equal(ethers.utils.parseEther('.5'))
     })
 
-    it('Should let participators confirm the loan', async () => {
-      await contract.confirmLoan(0)
-      const loan = await contract.getLoan(0)
-
-      expect(loan.confirmations).to.equal(1)
-    })
-
-    it('Should let participators revoke confirmation', async () => {
-      await contract.confirmLoan(0)
-      await contract.revokeConfirmation(0)
+    it('Should let lender confirm loan with loan deposit', async () => {
+      await contract.confirmLender(0, { value: ethers.utils.parseEther('.5') })
 
       const loan = await contract.getLoan(0)
-      expect(loan.confirmations).to.equal(0)
+      const contractBalance = await contract.getContractBalance()
+
+      expect(loan.lenderConfirmed).to.equal(true)
+      expect(contractBalance).to.equal(ethers.utils.parseEther('.5'))
     })
 
+    it('Should let borrower confirm loan with collateral deposit', async () => {
+      await collateral.connect(user2).mint()
+
+      await collateral.connect(user2).setApprovalForAll(contract.address, true)
+      await contract.connect(user2).confirmBorrower(0)
+
+      const loan = await contract.getLoan(0)
+      const collateralBalance = await collateral.balanceOf(contract.address)
+
+      expect(loan.borrowerConfirmed).to.equal(true)
+      expect(collateralBalance).to.equal(1)
+    })
+
+    it('Should activate loan as both parties confirm the loan', async () => {
+      // Confirming lender
+      await contract.confirmLender(0, { value: ethers.utils.parseEther('.5') })
+
+      // Confirming borrower
+      await collateral.connect(user2).mint()
+      await collateral.connect(user2).setApprovalForAll(contract.address, true)
+      await contract.connect(user2).confirmBorrower(0)
+
+      const loan = await contract.getLoan(0)
+      expect(loan.active).to.equal(true)
+    })
+
+    // it('Should not let third party confirm loan', async () => {
+    //   await expect(contract.connect(user3).confirmLender(0)).to.be.revertedWith('You are not the lender of this loan')
+    // })
   })
-
 });
