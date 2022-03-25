ab8639f5d01cee28fbb98cbccdfce890cdce9d9f
benbaessler <mail@benbaessler.com>
2022-03-25 21:51:37 +0000

    Past deadline tests

--- test/test.js
+++ test/test.js
@@ -17,19 +17,17 @@ describe("LoanFactory contract", () => {
 
     const Collateral = await ethers.getContractFactory('Collateral') 
     collateral = await Collateral.deploy()
+
+    await contract.submitLoan(
+      user1.address, 
+      user2.address, 
+      ethers.utils.parseEther('.5'),
+      { contractAddress: collateral.address, tokenId: 1 },
+      1650572328
+    )
   })
 
   describe('Initiating a loan', () => {
-
-    beforeEach(async () => {
-      await contract.submitLoan(
-        user1.address, 
-        user2.address, 
-        ethers.utils.parseEther('.5'),
-        { contractAddress: collateral.address, tokenId: 1 },
-        1234567
-      )
-    })
 
     it('Should be able to submit a loan', async () => {
       const loan = await contract.getLoan(0)
@@ -50,7 +48,7 @@ describe("LoanFactory contract", () => {
     })
 
     it('Should let borrower confirm loan with collateral deposit', async () => {
-      await collateral.connect(user2).mint()
+      await collateral.connect(user2).mint(1)
 
       await collateral.connect(user2).setApprovalForAll(contract.address, true)
       await contract.connect(user2).confirmBorrower(0)
@@ -62,35 +60,36 @@ describe("LoanFactory contract", () => {
       expect(collateralBalance).to.equal(1)
     })
 
-    it('Should not let third party confirm loan', async () => {
-      await expect(contract.connect(user3).confirmLender(0)).to.be.revertedWith('You are not the lender of this loan')
-      await expect(contract.connect(user3).confirmBorrower(0)).to.be.revertedWith('You are not the borrower of this loan')
-    })
-  })
-
-  describe('Executing a loan', () => {
-
-    beforeEach(async () => {
-      await contract.submitLoan(
-        user1.address, 
-        user2.address, 
-        ethers.utils.parseEther('.5'),
-        { contractAddress: collateral.address, tokenId: 1 },
-        1234567
-      )
-
+    it('Should activate loan as both parties confirm loan', async () => {
       // Confirming lender
       await contract.confirmLender(0, { value: ethers.utils.parseEther('.5') })
 
       // Confirming borrower
-      await collateral.connect(user2).mint()
+      await collateral.connect(user2).mint(1)
       await collateral.connect(user2).setApprovalForAll(contract.address, true)
       await contract.connect(user2).confirmBorrower(0)
-    })
 
-    it('Should activate loan as both parties confirm loan', async () => {
       const loan = await contract.getLoan(0)
       expect(loan.active).to.equal(true)
+    })
+
+    it('Should not let third party confirm loan', async () => {
+      await expect(contract.connect(user3).confirmLender(0)).to.be.revertedWith('You are not the lender of this loan')
+      await expect(contract.connect(user3).confirmBorrower(0)).to.be.revertedWith('You are not the borrower of this loan')
+    })
+
+  })
+
+  describe('Paying back a loan', () => {
+
+    beforeEach(async () => {
+      // Confirming lender
+      await contract.confirmLender(0, { value: ethers.utils.parseEther('.5') })
+
+      // Confirming borrower
+      await collateral.connect(user2).mint(1)
+      await collateral.connect(user2).setApprovalForAll(contract.address, true)
+      await contract.connect(user2).confirmBorrower(0)
     })
 
     it('Should let borrower pay back loan', async () => {
@@ -120,6 +119,51 @@ describe("LoanFactory contract", () => {
 
       expect(loan.executed).to.equal(true)
       expect(loan.active).to.equal(false)
+    })
+
+  })
+
+  describe('Past deadline', async () => {
+
+    beforeEach(async () => {
+      await contract.submitLoan(
+        user1.address, 
+        user2.address, 
+        ethers.utils.parseEther('.5'),
+        { contractAddress: collateral.address, tokenId: 1 },
+        1648156728
+      )
+
+      // Confirming lender
+      await contract.confirmLender(1, { value: ethers.utils.parseEther('.5') })
+
+      // Confirming borrower
+      await collateral.connect(user2).mint(1)
+      await collateral.connect(user2).setApprovalForAll(contract.address, true)
+      await contract.connect(user2).confirmBorrower(1)
+    })
+
+    it('Should let lender claim collateral', async () => {
+      await contract.claimCollateral(1)
+
+      const userCollateralBalance = await collateral.balanceOf(user1.address)
+      const contractCollateralBalance = await collateral.balanceOf(contract.address)
+
+      expect(userCollateralBalance).to.equal(1)
+      expect(contractCollateralBalance).to.equal(0)
+    })
+
+    it('Should execute loan after collateral claim', async () => {
+      await contract.claimCollateral(1)
+
+      const loan = await contract.getLoan(1)
+
+      expect(loan.executed).to.equal(true)
+      expect(loan.active).to.equal(false)
+    })
+
+    it('Should not let borrower pay back loan', async () => {
+      await expect(contract.connect(user2).paybackLoan(1, { value: ethers.utils.parseEther('.5') })).to.be.revertedWith('Past deadline')
     })
 
   })
