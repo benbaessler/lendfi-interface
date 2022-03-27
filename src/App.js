8bfa0c15c4f7ff4dea8ba1377fdb02410a675c1a
benbaessler <mail@benbaessler.com>
2022-03-27 18:00:58 +0100

    Initialized front-end blockchain integration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import { useState } from 'react'
+import { Button } from 'react-bootstrap';
+import { providers } from 'ethers'
+import { useWeb3React } from "@web3-react/core"
+import { useEagerConnect, useInactiveListener } from './hooks'
+import { injected } from './connectors'
 
 function App() {
+  const { active, account, activate } = useWeb3React()
+
+  const connectWallet = async () => {
+    try { await activate(injected) }
+    catch (error) { console.error(error) }
+  }
+
+  const triedEager = useEagerConnect()
+  useInactiveListener(!triedEager)
+
   return (
     <div className="App">
+      <Button onClick={connectWallet}>Connect Wallet</Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+
+const getLibrary = (provider) => {
+  return new Web3Provider(provider)
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
