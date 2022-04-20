f036234846a8107e0acdf80a54dd32cc64c2cc41
benbaessler <mail@benbaessler.com>
2022-04-20 16:52:55 +0100

    Displaying Loan data

--- src/pages/Loans/index.tsx
+++ src/pages/Loans/index.tsx
@@ -1,6 +1,6 @@
 import './style.css'
 import { useWeb3React } from '@web3-react/core'
-import { providers, Contract } from 'ethers'
+import { providers, utils } from 'ethers'
 import { getContract } from '../../utils/contract'
 import { useEffect, useState } from 'react'
 import { Loan } from '../../types/loan'
@@ -20,36 +20,62 @@ Dashboard button
 interface LoanComponentProps { data: Loan }
 
 export default function Loans() {
-  const { active, library } = useWeb3React()
+  const { active, library, account } = useWeb3React()
   let provider: providers.Web3Provider
   let signer: providers.JsonRpcSigner
+  const [hasSigner, setHasSigner] = useState<boolean>(false)
 
-  const [loans, setLoans] = useState<Loan[]>()
+  const [loans, setLoans] = useState<Loan[]>([])
 
   const _getLoan = async () => {
     const factoryContract = getContract(signer)
     const loan = await factoryContract.getLoan(0)
-    console.log(loan)
+    setLoans([loan])
   }
 
   useEffect(() => {
     if (active) {
       provider = new providers.Web3Provider(library.provider)
       signer = provider.getSigner()
-      // _getLoan()
+      _getLoan()
     }
   }, [active])
-  const LoanComponent = () => <div className="loanContainer">
-    <div className="loanContentContainer">
-      <p>Type</p>
-      <p>User</p>
-      <p>Amount</p>
-      <p>Interest</p>
-      <p>Collateral</p>
-      <p>Status</p>
-      <p>Deadline</p>
-    </div>
-  </div>
+
+  useEffect(() => {
+
+  }, [hasSigner])
+
+  const LoanComponent = ({ data }: LoanComponentProps) => {    
+    const formattedDeadline = new Date(data.deadline * 1000)
+
+    let status: string
+    let statusColor: string
+
+    if (data.active) {
+      status = 'Active'
+      statusColor = '#14c443'
+    } else if (data.executed) {
+      status = 'Executed'
+      statusColor = '#ff0000'
+    } else {
+      status = 'Initialized'
+      statusColor = 'white'
+    }
+
+    return (
+      <div className="loanContainer">
+        <div className="loanContentContainer">
+          <p>{account === data.lender ? 'To' : 'From'}</p>
+          <p>{account === data.lender ? data.borrower : data.lender}</p>
+          <p>{utils.formatEther(data.amount)} ETH</p>
+          <p>{utils.formatEther(data.interest)}</p>
+          <p id="collateralViewBtn">View</p>
+          <p style={{ color: statusColor }}>{status}</p>
+          <p>{formattedDeadline.toLocaleString()}</p>
+        </div>
+      </div>
+    )
+  }
 
   return <div className="interfaceContainer">
     <h1 id="loanUiTitle">Your Loans</h1>
@@ -62,6 +88,6 @@ export default function Loans() {
       <p>Status</p>
       <p>Deadline</p>
     </div>
-    <LoanComponent/>
+    {loans.map((loan: Loan) => <LoanComponent data={loan}/>)}
   </div>
 }
