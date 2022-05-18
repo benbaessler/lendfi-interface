3252389cb83c57d55d65bbce93c7dddf163ce7da
benbaessler <mail@benbaessler.com>
2022-05-18 15:50:06 +0100

    Files updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import { useEagerConnect, useInactiveListener } from './hooks'
 import Create from './pages/Create';
 import Loans from './pages/Loans';
+import { Loan } from './pages/Loan';
 import NavigationBar from './components/NavigationBar';
 
 function App() {
@@ -10,10 +11,13 @@ function App() {
   const triedEager = useEagerConnect()
   useInactiveListener(!triedEager)
   
+  // Add Redirect from /loans to /
+
   return <Switch>
     <div className="App">
       <NavigationBar/>
       <Route path="/" exact component={Loans}/>
+      <Route path="/loan/:id" exact component={Loan}/>
       <Route path="/create" component={Create}/>
     </div>
   </Switch>
