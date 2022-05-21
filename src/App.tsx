d23c833b765d0cfea1c34e739120d991416c9e05
benbaessler <mail@benbaessler.com>
2022-05-21 16:56:45 +0100

    Created Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,19 @@ import { useEagerConnect, useInactiveListener } from './hooks'
 import Create from './pages/Create';
 import Loans from './pages/Loans';
 import About from './pages/About';
-import { LoanPage } from './pages/LoanPage';
+import { LoanPage } from './pages/Loan';
 import NavigationBar from './components/NavigationBar';
+import Landing from './pages/Landing';
 
 function App() {
   // Ethereum & Contract
   const triedEager = useEagerConnect()
   useInactiveListener(!triedEager)
   
-  // Add Redirect from /loans to /
   return <Switch>
     <div className="App">
       <NavigationBar/>
+      <Route path="/" exact component={Landing}/>
       <Route path="/loans" exact component={Loans}/>
       <Route path="/loan/:id" exact component={LoanPage}/>
       <Route path="/create" exact component={Create}/>
---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,8 @@
+import './style.css'
+
+export default function Landing() {
+  return <div className="landingContainer">
+    <h1>Welcome to Lendfi</h1>
+    <h4>Easily create private and autonomous loans with NFT collateral.</h4>
+  </div>
+}
