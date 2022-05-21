e281aa3cc68aec728b91a603f506441b1244cf45
benbaessler <mail@benbaessler.com>
2022-05-21 17:36:17 +0100

    Designed Landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,8 +1,19 @@
 import './style.css'
+import { useContext } from 'react'
+import { EnteredContext } from '../../state/appEntered'
+import LandingImage from '../../assets/images/landing-animation.png'
 
 export default function Landing() {
+  const [appEntered, setAppEntered] = useContext(EnteredContext)
+
   return <div className="landingContainer">
-    <h1>Welcome to Lendfi</h1>
-    <h4>Easily create private and autonomous loans with NFT collateral.</h4>
+    <div className="landingWelcomeContainer">
+      <h1>Welcome to <span id="welcomeTitle">LendFi</span></h1>
+      <h6>Easily lend or borrow crypto using fully secure and autonomous smart contracts with NFT collateral.</h6>
+      <div className="button submitButton" id="landingEnterBtn">Get Started</div>
+    </div>
+    <div className="landingBrandingContainer">
+      <img src={LandingImage}/>
+    </div>
   </div>
 }
